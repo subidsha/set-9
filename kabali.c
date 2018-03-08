08ebe3fbd07c2742f2ece666f2b59e203ea2ecf0
subidsha <31504698+subidsha@users.noreply.github.com>
2018-03-08 16:04:09 +0530

    Create kabali.c

---
+++ kabali.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+	int a,c,b;
+	scanf("%d\t%d",&a,&b);
+	if(a>b)
+	{
+		printf("the difference of two clans:%d",a-b);
+	}
+	else
+	{
+	        	printf("the difference of two clans:%d",b-a);	
+	}
+	return 0;
+}
