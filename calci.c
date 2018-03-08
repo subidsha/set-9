9c0dd3ca2163b3d072fd4eefc9e6bb23040674b2
subidsha <31504698+subidsha@users.noreply.github.com>
2018-03-08 16:20:41 +0530

    Create calci.c

---
+++ calci.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+  int a,b;
+  char ch;
+  scanf("%d%c%d",&a,&ch,&b);
+  switch(ch)
+  {
+  case '/':
+     printf("%d",a/b);
+     break;
+  case '%':
+      printf("%d",a%b);
+      break;
+  default:
+      printf("no operations");
+      break;
+  }
+  return 0;
+}
